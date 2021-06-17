85a6328552efb9af855393689c325b3925e6e52d
Piero <piero.onorati@virgilio.it>
2021-06-17 19:09:52 +0200

    complete exercise

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ Milestone 3
 Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio, inserendo solamente nome, colore e opacità del fiocco per ogni gatto. */
 
 
+
+/* Milestone 1*/
+// Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
 const gatti = [
     {
         nome:'Tom',
@@ -29,13 +32,84 @@ const gatti = [
     {
         nome:'Pallina',
         eta : 4,
-        colore: '#4e3b31',
+        colore: '#8a6642',
         sesso : 'F'
     }
 ];
 
-
-gatti.forEach(element => 
+// Tramite il forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
+gatti.forEach((element) => 
     document.getElementById('gatti').innerHTML+=
     `${element.nome}: <i class="fas fa-cat" style="color:${element.colore};"></i> <br>`
+);
+
+
+/* Milestone 2*/
+// Creiamo un nuovo array con tutti gli elementi di quello di partenza + il fiocco
+
+const rosa = '#ff1493';
+const blu = '#0000ff';
+
+const nuoviGatti = gatti.map((element) => {
+
+    // const nome = element.nome;
+    // const eta = element.eta;
+    // const colore = element.colore;
+    // const sesso = element.sesso;
+
+    const{nome, eta, colore, sesso} = element;
+
+    let coloreFiocco = (sesso == "F")? rosa : blu;
+    let opacita = element.eta/4;
+
+    return{
+        nome,
+        eta,
+        colore,
+        sesso,
+        fiocco: {
+            colore: coloreFiocco,
+            opacita: opacita
+        }
+    }
+});
+
+
+// Dividiamo i maschi dalle femmine
+
+const gattiMaschi = nuoviGatti.filter((element) => {
+    return element.sesso == 'M';
+});
+
+
+gattiMaschi.forEach((element) => 
+    document.getElementById('gattiMaschi').innerHTML+=
+    `${element.nome}: <i class="fas fa-cat" style="color:${element.colore};"></i> <i class="fas fa-award" style="color:${element.fiocco.colore}; opacity:${element.fiocco.opacita}"></i><br>
+    `
+);
+
+
+const gattiFemmine = nuoviGatti.filter((element) => {
+    return element.sesso == 'F';
+});
+
+gattiFemmine.forEach((element) => 
+    document.getElementById('gattiFemmine').innerHTML+=
+    `${element.nome}: <i class="fas fa-cat" style="color:${element.colore};"></i> <i class="fas fa-award" style="color:${element.fiocco.colore}; opacity:${element.fiocco.opacita}"></i><br>
+    `
+);
+
+/* Milestone 3 */
+// Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio
+const gattiFemmineMaschi = [...gattiFemmine,...gattiMaschi];
+
+const gattiFemmineMaschiZip = gattiFemmineMaschi.map((element)=>{
+    const { nome, colore, fiocco} = element;
+    return { nome, colore, fiocco}
+});
+
+gattiFemmineMaschiZip.forEach((element) => 
+    document.getElementById('arrayUnico').innerHTML+=
+    `${element.nome}: <i class="fas fa-cat" style="color:${element.colore};"></i> <i class="fas fa-award" style="color:${element.fiocco.colore}; opacity:${element.fiocco.opacita}"></i><br>
+    `
 );
